33530c6dda33cff71e5aa6f69dfc5e51dfe9b9ae
Manuel Alabor <msites@msites.net>
2014-12-31 15:59:26 +0100

    better runner

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 var orchestraApi = require('orchestra-api')
 	, orchestraClient = require('orchestra-client')
-	, express = require('express')
-	, app = express();
+	, app = require('express')();
 
 app.use('/', orchestraClient);
 app.use('/api', orchestraApi);
+app.listen(8080, '127.0.0.1', function() {
+	console.log('listen');
+	require('node-thrust')(function(err, api) {
+		var window = api.window({
+			root_url: 'http://127.0.0.1:8080'
+			, size: {
+				width: 1024
+				, height: 768
+			}
+		});
+		
+		window.on('closed', function() {
+			process.exit(0);
+		});
 
-app.listen(8080);
-
-/*
-require('node-thrust')(function(err, api) {
-
-	var window = api.window({
-		root_url: 'http://google.com'
-		, size: {
-			width: 1024
-			, height: 768
-		}
+		window.open_devtools(function() {
+			window.show();
+		});
 	});
 	
-	window.on('closed', function() {
-		process.exit(0);
-	});
-	
-	window.show();
 });
-*/
