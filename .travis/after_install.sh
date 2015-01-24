c9d2bb4bf0329177e8d3e437138ba8eaf5006509
Manuel Alabor <msites@msites.net>
2015-01-24 01:30:08 +0100

    publish to orchestra-builds

--- .travis/after_install.sh
+++ .travis/after_install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Prepare the client:
+echo '---- Prepare orchestra-client for nw.js ----'
 ( cd node_modules/orchestra-client && node_modules/.bin/gulp clean prepare-node-webkit )
 
 # Rebuild native modules:
-NW_VERSION="0.11.6"
+echo '---- Rebuild native node modules using nw-gyp ----'
 npm install nw-gyp -g
 ( cd node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-stringprep && nw-gyp configure --target=$NW_VERSION && nw-gyp build )
 ( cd node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-xmpp-core/node_modules/ltx/node_modules/node-expat && nw-gyp configure --target=$NW_VERSION && nw-gyp build )
---
+++ .travis/after_success.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo '---- Checkout orchestra-builds/gh-pages ----'
+cd /tmp
+git clone --depth=1 --branch=gh-pages https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} gh-pages 2>&1
+cd gh-pages
+
+echo '---- Create directory builds/$TRAVIS_COMMIT ----'
+mkdir -p builds/$TRAVIS_COMMIT
+
+echo '---- Copy build ----'
+cp $ORCHESTRA_BUILD ./$TRAVIS_COMMIT
+
+echo '---- Set git settings ----'
+git config --global user.name $GIT_AUTHOR_NAME
+git config --global user.email $GIT_AUTHOR_EMAIL
+git config --global push.default matching
+
+echo '---- Add files, commit and push ----'
+git add -A
+git commit -m "adding build for revision $TRAVIS_COMMIT"
+git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
