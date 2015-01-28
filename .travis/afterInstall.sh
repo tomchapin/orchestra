d1548198b112c5fe43116edabd598a020e8730a8
Manuel Alabor <msites@msites.net>
2015-01-28 19:46:47 +0100

    tinker with array syntax for afterInstall.sh

--- .travis/afterInstall.sh
+++ .travis/afterInstall.sh
@@ -11,13 +11,13 @@ echo "---- Prepare orchestra-client for nw.js ----"
 ( cd node_modules/orchestra-client && node_modules/.bin/gulp clean prepare-node-webkit )
 
 # Rebuild native modules:
-echo "---- Rebuild native node modules using nw-gyp for nw $NW_VERSION ----"
+echo "---- Rebuild native node modules using nw-gyp for nw.js $NW_VERSION ----"
 npm install nw-gyp -g
 
-NATIVE_MODULE_PATHS=(
-	"node_modules/orchestra-client/node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-stringprep"
-	"node_modules/orchestra-client/node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-xmpp-core/node_modules/ltx/node_modules/node-expat"
-	"node_modules/orchestra-client/node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-xmpp-core/node_modules/node-stringprep"
+declare -a NATIVE_MODULE_PATHS=(
+	'node_modules/orchestra-client/node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-stringprep'
+	'node_modules/orchestra-client/node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-xmpp-core/node_modules/ltx/node_modules/node-expat'
+	'node_modules/orchestra-client/node_modules/orchestra-jsapi/node_modules/harmonyhubjs-client/node_modules/node-xmpp-core/node_modules/node-stringprep'
 )
 
 for NATIVE_MODULE_PATH in ${NATIVE_MODULE_PATHS[@]}
